0f67670d6ea02b550cf9f7eb82e55cf5a2b4d5c7
agavras <math.falze@gmail.com>
2022-05-04 11:48:47 +0200

    optimisation webp

--- overeffect.js
+++ overeffect.js
@@ -1,7 +1,7 @@
 let hoverDistort = new hoverEffect({
     parent: document.querySelector(".wrapper"),
     intensity: 0.5,
-    image1: "./imgProjets/github1.jpg",
-    image2: "./imgProjets/github2.jpg",
-    displacementImage: "./imgEffets/4.png"
+    image1: "./imgProjets/github1.webp",
+    image2: "./imgProjets/github2.webp",
+    displacementImage: "./imgEffets/4.webp"
 });
