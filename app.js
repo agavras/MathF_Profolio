53d2c492722ff6576189e1140782c9771248bb50
agavras <math.falze@gmail.com>
2022-05-06 09:02:49 +0200

    optimisation pour validation

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-const pcCanva = document.getElementById("application-canvas");
-// console.log(pcCanva);
 const modal1 = document.getElementById('modal1');
 const modal2 = document.getElementById('modal2');
 const modal3 = document.getElementById('modal3');
 
-pcCanva.className="animate__animated animate__fadeIn animate__1"
-
-pcCanva.addEventListener("mouseover", () => {
-    document.body.style.cursor = "pointer";
+//Rive anim page contact
+new rive.Rive({
+    src: '/morphtest.riv',
+    canvas: document.getElementById('canvas'),
+    autoplay: true,
+    layout: new rive.Layout({fit: 'none', alignment: 'center'}),
 })
 
-pcCanva.addEventListener("mouseleave", () => {
-    document.body.style.cursor = "auto";
-})
-
-pcCanva.addEventListener("click", () => {
-    window.open("#projets", "_self").focus();
-})
-
+//affiche la modal projet 1 ou 2 ou 3
 function showProject(projetID) {
     if (projetID === 1) {
         modal1.className="animate__animated animate__fadeIn animate__1"
@@ -35,6 +28,7 @@ function showProject(projetID) {
     }
 }
 
+//cache la modal projet 1 ou 2 ou 3
 function hideProject(projetID) {
     if (projetID === 1) {
         modal1.className="empty"
@@ -49,19 +43,5 @@ function hideProject(projetID) {
     if (projetID === 3) {
         modal3.className="empty"
         modal3.style.display="none";
-    }
-}
-
-function goto(projetID) {
-    if (projetID === 1) {
-        window.open("https://agavras.github.io/BUILD/index.html", "_blank").focus();
-    }
-
-    if (projetID === 2) {
-        window.open("https://agavras.github.io/EmbeddedBrowser/index.html", "_blank").focus();
-    }
-
-    if (projetID === 3) {
-        window.open("https://agavras.github.io/Impression/index.html", "_blank").focus();
     }
 }
